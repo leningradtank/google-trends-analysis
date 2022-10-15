67621a0f149486efc76c7d2ce6828beb930d256a
ridersamz2000 <47441021+ridersamz2000@users.noreply.github.com>
2022-10-15 19:29:31 +0700

    new v

--- pytrends_connect.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pytrends.request import TrendReq
-
-pytrend = TrendReq()
-
-# Get Google Keyword Suggestions
-keywords = pytrend.suggestions(keyword='covid')
-df = pd.DataFrame(keywords)
-print(df.head(5))
